cac351fc7149d2da922129636f2deadcf1fc24e9
Lucas Charoing <charoing.lucas@gmail.com>
2022-10-12 00:22:48 +0200

    Ajout des skills avec boutons fonctionnels

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 let studiesBtn = document.querySelectorAll('.about-me--content--buttons button');
 let studiesTxt = document.querySelectorAll('.about-me--content--text p');
+let skillsBtn = document.querySelectorAll('.about-me__second-page--content--buttons button');
+let skillsTxt = document.querySelectorAll('.about-me__second-page--content--text p');
 
 for (let i = 0; i < studiesBtn.length; i++) {
     studiesBtn[i].addEventListener('click', () => {
@@ -9,6 +11,19 @@ for (let i = 0; i < studiesBtn.length; i++) {
             }
             else {
                 study.style.opacity = 0;
+            }
+        })
+    })
+}
+
+for (let i = 0; i < skillsBtn.length; i++) {
+    skillsBtn[i].addEventListener('click', () => {
+        skillsTxt.forEach(skill => {
+            if(skill == skillsTxt[i+1]) {
+                skill.style.opacity = 1;
+            }
+            else {
+                skill.style.opacity = 0;
             }
         })
     })
